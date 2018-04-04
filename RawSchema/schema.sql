c9663af15c0122eb6ca49bf97dcbc5edfc6085d2
daneyal10 <daneyalb254@gmail.com>
2018-04-04 18:07:41 -0400

    fix rater table

--- RawSchema/schema.sql
+++ RawSchema/schema.sql
@@ -4,15 +4,9 @@ CREATE TABLE Rater (
     UserID INTEGER,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL UNIQUE,
-<<<<<<< HEAD
     signup_day DATE NOT NULL,
     type INTEGER NOT NULL,
-    reputation DECIMAL(1,2) NOT NULL,
-=======
-    signup_day DATE NOT NULL, 
-    type INTEGER NOT NULL, 
     reputation INTEGER NOT NULL,
->>>>>>> a6cfd0f0826cb1ed4324192b8b14a74e105ebe57
     PRIMARY KEY (UserID),
     CONSTRAINT reputation_bound CHECK (reputation >=0 AND reputation <=5),
     FOREIGN KEY (type) REFERENCES RaterCredibility(type)
